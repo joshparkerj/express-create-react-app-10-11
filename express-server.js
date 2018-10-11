b7f2a09eb335f7add7621f9de81033dc9963bebd
joshparkerj <superfakek@gmail.com>
2018-10-11 02:04:58 -0600

    standardize the port number a bit

--- express-server.js
+++ express-server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static('./build'));
 app.get('/',(req,res) => {
   res.sendFile('./build/index.html');
 })
 
-app.listen(80,() => {
-  console.log('now listening on port 80')
+app.listen(port,function () {
+  console.log(`now listening on ${port}`)
 })
